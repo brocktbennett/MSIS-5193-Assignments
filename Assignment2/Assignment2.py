f42dccc5746ae2b7eb7b1c62e7484dcb41293279
Brock T. Bennett <brock.bennett@okstate.edu>
2023-09-13 18:18:18 -0500

    Started assignment 2 by writing out some structural pseaudo code.
    Signed-off-by: Brock T. Bennett <brock.bennett@okstate.edu>

---
+++ Assignment2/Assignment2.py
@@ -0,0 +1,23 @@
+# Import Library
+import pandas as pd
+
+# use the data: 2017CHR_CSV_Analytic_Data-new.csv
+
+# reference the data
+pd_CHR = pd.read_csv("2017CHR_CSV_Analytic_Data-new.csv ")
+
+# 1: Check to see if any columns have missing values (show the results).
+# If there are columns having missing value, impute the columns (with mising values).
+# If there is no missing value, leave the data frame as is.
+
+# 2: Drop(remove) the identifier columns: 5-Digit FIPS Code, statecode, countycode, county(5 points)
+
+
+
+# 3: Use z-score normalization to normalize these columns: Poor Physical health days
+#
+
+
+
+
+
